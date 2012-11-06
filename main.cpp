5e2721f5a22991f31a8b2eef1e96372dbf88fadc
unknown <benoit.averty@gmail.com>
2012-11-06 16:13:39 +0100

    Added empty main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//////////////////////////////////////////////////////////////////////////
+// Module IRF, 5-iNFO
+// Projet
+//////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+
+#include <opencv2\opencv.hpp>
+#include <opencv2\core\core.hpp>
+#include <opencv2\highgui\highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main (int argc, char** argv) {
+
+}
